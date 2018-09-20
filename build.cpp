eae21140a35d1b2e25d2d19310b76dd621666918
DuaneShaffer <dlshaffer@alaska.edu>
2018-09-20 09:37:50 -0800

    Initial declaration of needed function. Builds and runs

--- build.cpp
+++ build.cpp
@@ -8,3 +8,11 @@
 
 #include "build.hpp"
 
+#include <vector>
+using std::vector;
+
+using Bridge = vector<int>;
+
+int build(int w, int e, const vector<Bridge> & bridges) {
+    return 0;
+}
--- build.hpp
+++ build.hpp
@@ -9,11 +9,10 @@
 #ifndef BUILD_HPP
 #define BUILD_HPP
 
+#include <vector>
 
+using Bridge = std::vector<int>;
 
-
-
-
-
+int build(int w, int e, const std::vector<Bridge> & bridges);
 
 #endif //BUILD_HPP
