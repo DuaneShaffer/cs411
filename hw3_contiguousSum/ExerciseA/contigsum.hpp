bac49d25bd4a1d38d49c4edcd08d0886e125e74a
DuaneShaffer <dlshaffer@alaska.edu>
2018-10-11 13:45:13 -0800

    Restructure to a folder for each exercise

--- hw3_contiguousSum/ExerciseA/contigsum.hpp
+++ hw3_contiguousSum/ExerciseA/contigsum.hpp
@@ -1,8 +1,12 @@
-//
-// Created by Duane on 10/11/2018.
-//
+// contigsum.hpp
+// Duane Shaffer
+// CS411
+// 10/11/2018
 
 #ifndef HW3_CONTIGSUM_HPP
 #define HW3_CONTIGSUM_HPP
+
+
+
 
 #endif //HW3_CONTIGSUM_HPP
