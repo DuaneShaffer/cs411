366d440699a722f73cf5df69f8fefd74b7644690
DuaneShaffer <dlshaffer@alaska.edu>
2018-10-11 16:17:32 -0800

    impliment exercise A

--- hw3_contiguousSum/ExerciseA/contigsum.hpp
+++ hw3_contiguousSum/ExerciseA/contigsum.hpp
@@ -2,9 +2,62 @@
 // Duane Shaffer
 // CS411
 // 10/11/2018
+#include <algorithm>
 
 #ifndef CONTIGSUM_HPP
 #define CONTIGSUM_HPP
+
+struct GCS{
+    int gcs = 0;
+    int sumFirst = 0;
+    int sumLast = 0;
+    int total = 0;
+};
+
+GCS merge(GCS & left, GCS & right){
+    GCS mergedGCS;
+    mergedGCS.gcs = std::max({left.gcs, right.gcs, (left.sumLast + right.sumFirst)});
+    mergedGCS.sumFirst = std::max(left.sumFirst, left.total + right.sumFirst);
+    mergedGCS.sumLast = std::max(right.sumLast, right.total + left.sumLast);
+    mergedGCS.total = right.total + left.total;
+    return mergedGCS;
+
+}
+
+template<typename RAIter>
+GCS gcsRecurse(RAIter first, RAIter last){
+    GCS gcs;
+
+    if(distance(first, last) == 1){
+        gcs.gcs = *first;
+        gcs.sumFirst = *first;
+        gcs.sumLast = *first;
+        gcs.total = *first;
+        return gcs;
+    }
+
+    auto middle = first;
+    advance(middle,distance(first,last)/2);
+    auto left_gcs = gcsRecurse(first, middle);
+    auto right_gcs = gcsRecurse(middle, last);
+
+    return (merge(left_gcs, right_gcs));
+
+}
+
+
+template<typename RAIter>
+int contigSum(RAIter first, RAIter last) {
+    if(distance(first,last) == 0){
+        return 0;
+    }
+    auto gcsTest = gcsRecurse(first, last);
+    return std::max(gcsTest.gcs, 0);
+}
+
+
+
+
 
 
 
