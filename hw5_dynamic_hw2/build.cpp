1addd41616dfe6496f2f87d9e3b0c8b2f9b22606
DuaneShaffer <dlshaffer@alaska.edu>
2018-11-19 14:10:39 -0900

    sort the adjacency list

--- hw5_dynamic_hw2/build.cpp
+++ hw5_dynamic_hw2/build.cpp
@@ -14,19 +14,47 @@ using std::vector;
 #include <algorithm>
 using std::sort;
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 using Bridge = vector<int>;
 
 int build(int w, int e, const vector<Bridge> & bridges) {
     auto bridges_copy = bridges;
 
-    sort(bridges_copy.begin(),bridges_copy.end(), [](const Bridge & a, const Bridge & b) -> bool {
-        if (a[0] < b[0])
-            return true;
-        else if (a[0] == b[0])
-            return a[1] < b[1];
-        else
-            return false;
-    });
+    vector<vector<Bridge>> adj_list(w);
+    for (auto i: bridges) {
+        adj_list[i[0]].push_back(Bridge{i[1],i[2],0});
+    }
+
+    for (unsigned int i = 0; i < adj_list.size(); ++i) {
+        sort(adj_list[i].begin(),adj_list[i].end(), [](const Bridge & a, const Bridge & b) -> bool {
+            if (a[0] < b[0])
+                return true;
+            else if (a[0] == b[0]) {
+                if (a[1] > b[1])
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        });
+    }
+
+    for (unsigned int i = 0; i < adj_list.size(); ++i) {
+        cout << i << ": ";
+        for (auto j: adj_list[i]) {
+            cout << j[0] << " ";    
+        }
+        cout <<  endl;
+    }
+
+    int max = 0;
+    int tempmax = 0;
+    int row = bridges_copy[0][0];
+    int column = bridges_copy[0][0];
 
     return 0;
 
